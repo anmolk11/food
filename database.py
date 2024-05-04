3c0abcf290143b6e7d2739ecf7aeab0afdcb7a75
anmolk11 <akcp.cpp@gmail.com>
2024-05-04 23:35:15 +0530

    food items inserted into db

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ def insert_item(item : str,type : str) -> None:
     try:
         conn = sqlite3.connect('data.db')
         cursor = conn.cursor()
-        insert_query = "INSERT INTO food_items (item,type) VALUES (?,?)"
+        insert_query = "INSERT INTO food_list (item,type) VALUES (?,?)"
         cursor.execute(insert_query, (item,type,))
         conn.commit()
         print("Items inserted successfully!")
@@ -17,6 +17,22 @@ def insert_item(item : str,type : str) -> None:
         conn.close()
 
 
+def get_items() -> list:
+    try:
+        conn = sqlite3.connect('data.db')
+        cursor = conn.cursor()
+        query = "SELECT * FROM food_list"
+        cursor.execute(query)
+        items = cursor.fetchall()
+        return items
+    
+    except sqlite3.Error as e:
+        print("Error:", e)
+
+    finally:
+        cursor.close()
+        conn.close()
+
 if __name__ == '__main__':
     week_days = ['Sev Tamatar','Ballor','Poha','Gawar Phali','Gobhi','Allu - Dry','Allu - Liquid','Khichdi','Sabudana','Kadhi','Baingan','Karela']
     week_end = ['Daal Bati/Bafla','Paneer','Allu Paratha']
@@ -25,6 +41,9 @@ if __name__ == '__main__':
         insert_item(item,'WD')
     for item in week_end:
         insert_item(item,'WE')
+    items = get_items()
+    for item in items:
+        print(item)
 
 
 
